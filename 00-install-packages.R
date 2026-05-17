1c055d89fd23f08fd3bef9ecf0b7ee09826237a5
Quang Huynh <quang@bluematterscience.com>
2026-05-16 23:21:07 -0700

    Plot 7 area model for comparison

--- 00-install-packages.R
+++ 00-install-packages.R
@@ -3,5 +3,5 @@
 install.packages("tidyverse")
 install.packages("readxl")
 
-# May 7, 2026
-remotes::install_github("Blue-Matter/multiSA", ref = "dev2")
+# May 16, 2026
+remotes::install_github("Blue-Matter/multiSA", ref = "dev")
