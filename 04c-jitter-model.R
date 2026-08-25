e71f2369b7a1603e52c924aaf59c0023c08717a2
Quang Huynh <quang@bluematterscience.com>
2026-08-24 22:50:05 -0700

    Increase jitter amount

--- 04c-jitter-model.R
+++ 04c-jitter-model.R
@@ -11,10 +11,10 @@ for (j in 1:length(model_name)) {
   fit <- readRDS(file.path("model_output", paste0("fit_", i)))
 
   tictoc::tic()
-  fits <- do_jitter(fit, n = 10, amount = 0.25, seed = 23, cores = 5, use_fitted = FALSE, return_models = TRUE, do_sd = TRUE)
+  fits <- do_jitter(fit, n = 10, amount = 0.5, seed = 23, cores = 5, use_fitted = FALSE, return_models = TRUE, do_sd = TRUE)
   tictoc::toc()
 
-  saveRDS(fits, file = file.path("jitter", paste0("jitter_", i)))
+  saveRDS(fits, file = file.path("jitter", paste0("jitter_model_", i)))
 
   # Need time to compute max gradient
   like <- do.call(rbind, lapply(fits, get_likelihood_components))
@@ -26,22 +26,24 @@ if (FALSE) {
   i <- Design$output_name[j]
   like <- readRDS(file = file.path("jitter", paste0("jitter_loglik_", i, ".rds")))
 
+  fits <- readRDS(file = file.path("jitter", paste0("jitter_", i, ".rds")))
+
   names_df <- data.frame(
     variable = c("loglike", "loglike_CAL_ymfr", "loglike_I_ymi", "loglike_SC_ymafr",
-                 "loglike_tag_mov_ymars", "logprior", "logprior_par",
+                 "loglike_tag_mov_ymars", #"logprior", "logprior_par",
                  "logprior_rdev_ys", "penalty", "objective", "maxgrad"),
     variable2 = c("All likelihoods", "Like: Length composition", "Like: Indices", "Like: SOO",
-                  "Like: Tags", "All priors", "Pr: spatial prior",
+                  "Like: Tags", #"All priors", "Pr: spatial prior",
                   "Pr: Rec devs", "Penalty", "Objective", "Max. Gradient")
   )
 
   g <- like %>%
     mutate(Jitter = 1:n()) %>%
     reshape2::melt(id.vars = "Jitter") %>%
-    filter(!variable %in% c("loglike_Cinit_mfr", "conv", "fn")) %>%
+    left_join(names_df) %>%
+    filter(!is.na(variable2), !variable %in% c("loglike_Cinit_mfr", "conv", "fn")) %>%
     mutate(value = ifelse(grepl("log", variable), -1 * value, value)) %>%
     #mutate(value = value - min(value), .by = variable) %>%
-    left_join(names_df) %>%
     mutate(variable2 = factor(variable2, names_df$variable2)) %>%
     ggplot(aes(Jitter, value)) +
     facet_wrap(vars(variable2), scales = "free_y") +
